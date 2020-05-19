a558e499688336ccbe9d76909077919044fd74ef
Todd Spainhour <toddspainhour@gmail.com>
2020-05-18 19:19:38 -0500

    initial components added

---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+        <h2>Inside your Navbar Component</h2>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
