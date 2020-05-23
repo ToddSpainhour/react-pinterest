c6c1be7776fc2ba917ec0118445cb55f9b204c41
Todd Spainhour <toddspainhour@gmail.com>
2020-05-23 09:28:14 -0500

    pins deleting

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Pin.scss';
 import pinShape from '../../helpers/propz/pinShape';
 
 class Pin extends React.Component {
   static propTypes = {
     pin: pinShape.pinShape,
+    removePin: PropTypes.func.isRequired,
   }
+
+  deletePinEvent = (e) => {
+    e.preventDefault();
+    const { pin, removePin } = this.props;
+    removePin(pin.id);
+};
 
   render() {
     const { pin } = this.props;
@@ -16,6 +24,7 @@ class Pin extends React.Component {
         <img className="card-img-top" src={pin.imageUrl} alt="pin" />
         <div className="card-body">
           <h5 className="card-title">{pin.title}</h5>
+          <button className="btn btn-danger" onClick={this.deletePinEvent}>X</button>
         </div>
       </div>
     </div>
