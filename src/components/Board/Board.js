cd9cb02282827073b75a965fe36b04d2162d0721
Todd Spainhour <toddspainhour@gmail.com>
2020-05-23 10:52:23 -0500

    smash function completely removes boards and pins

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,13 +7,24 @@ class Board extends React.Component {
   static propTypes = {
     board: boardShape.boardShape,
     setSingleBoard: PropTypes.func.isRequired,
+    removeBoard: PropTypes.func.isRequired,
   }
+
 
 openSingleBoardEvent = (e) => {
   e.preventDefault();
   const { board, setSingleBoard } = this.props;
   setSingleBoard(board.id);
 }
+
+
+deleteBoardEvent = (e) => {
+  e.preventDefault();
+  console.error('you just ran the deleteBoardEvent');
+  const { board, removeBoard } = this.props;
+  removeBoard(board.id);
+}
+
 
 render() {
   const { board } = this.props;
@@ -25,6 +36,7 @@ render() {
       <h5 className="card-title">{board.name}</h5>
         <p className="card-text">{board.description}</p>
         <button className="btn btn-dark" onClick={this.openSingleBoardEvent}>View Pins</button>
+        <button className="btn btn-danger" onClick={this.deleteBoardEvent}>X</button>
     </div>
   </div>
 </div>
--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import boardsData from '../../helpers/data/boardsData';
 import authData from '../../helpers/data/authData';
 import Board from '../Board/Board';
+import smash from '../../helpers/data/smash';
 
 import './BoardContainer.scss';
 
@@ -16,28 +17,37 @@ class BoardContainer extends React.Component {
     boards: [],
   }
 
-  componentDidMount() {
-    boardsData.getBoardsByUid(authData.getUid())
-      .then((boards) => this.setState({ boards }))
-      .catch((err) => console.error('unable to get boards ', err));
-  }
+getAllBoards = () => {
+  boardsData.getBoardsByUid(authData.getUid())
+    .then((boards) => this.setState({ boards }))
+    .catch((err) => console.error('unable to get boards ', err));
+}
+
+componentDidMount() {
+  this.getAllBoards();
+}
 
 
-  render() {
-    const { boards } = this.state;
-    const { setSingleBoard } = this.props;
+removeBoard = (boardId) => {
+  smash.completlyRemoveBoard(boardId)
+    .then(() => this.getAllBoards())
+    .catch((err) => console.error('cannot delete full board', err));
+}
 
+render() {
+  const { boards } = this.state;
+  const { setSingleBoard } = this.props;
 
-    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
-    return (
-      <div className="BoardContainer">
-        <h2>Boards</h2>
-        <div className="d-flex flex-wrap">
-          {makeBoards}
-        </div>
+  const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard}/>);
+  return (
+    <div className="BoardContainer">
+      <h2>Boards</h2>
+      <div className="d-flex flex-wrap">
+        {makeBoards}
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default BoardContainer;
